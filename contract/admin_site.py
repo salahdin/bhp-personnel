24cf194f18668325075d07d27b3d2ecb26cfc6a2
ame diphoko <diphokoame@gmail.com>
2020-09-03 14:25:49 +0200

    disable admin nav sidebar

--- contract/admin_site.py
+++ contract/admin_site.py
@@ -5,6 +5,7 @@ from django.contrib.sites.shortcuts import get_current_site
 class AdminSite(DjangoAdminSite):
 
     site_url = '/administration/'
+    enable_nav_sidebar = False
 
     def each_context(self, request):
         context = super().each_context(request)
