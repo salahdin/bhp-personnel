85d268fbd8c1aa024f75fdc98fa207db4bc3cead
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-02-12 01:03:15 +0200

    Add list data

---
+++ contract/list_data.py
@@ -0,0 +1,14 @@
+from edc_list_data import PreloadData
+
+
+list_data = {
+    'contract.studies': [
+        ('potlako', 'Potlako'),
+        ('ambition', 'Ambition'),
+        ('cancer', 'Cancer'),
+        ('flourish', 'Flourish'),
+    ],
+}
+
+preload_data = PreloadData(
+    list_data=list_data)
