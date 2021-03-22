a6819bd1a74624af31f983f68b355c29341cc5c2
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-03-22 12:30:46 +0200

    Update performance assessment form

--- contract/admin/performance_assessment_admin.py
+++ contract/admin/performance_assessment_admin.py
@@ -2,25 +2,22 @@ from django.db import models
 from django.forms import Textarea
 
 from django.contrib import admin
+from django.urls.base import reverse
+from django.urls.exceptions import NoReverseMatch
 from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
 from edc_base.sites.admin import ModelAdminSiteMixin
-from edc_metadata import NextFormGetter
 from edc_model_admin import (
     ModelAdminNextUrlRedirectMixin, ModelAdminFormInstructionsMixin,
     ModelAdminFormAutoNumberMixin, ModelAdminAuditFieldsMixin,
     ModelAdminReadOnlyMixin, ModelAdminInstitutionMixin,
     ModelAdminRedirectOnDeleteMixin)
-from edc_model_admin import TabularInlineMixin
+from edc_model_admin import ModelAdminNextUrlRedirectError
 from edc_model_admin.model_admin_audit_fields_mixin import (
     audit_fieldset_tuple)
 
 from ..admin_site import contract_admin
 from ..forms import PerformanceAssessmentForm
-from ..forms import (CareerDevelopmentForm, ImprovementPlanForm,
-                     PerformanceAssessmentItemForm)
-from ..models import (CareerDevelopment,
-                      ImprovementPlan, PerformanceAssessment)
-from ..models import PerformanceAssessmentItem
+from ..models import PerformanceAssessment
 
 
 class ModelAdminMixin(ModelAdminNextUrlRedirectMixin,
@@ -34,60 +31,21 @@ class ModelAdminMixin(ModelAdminNextUrlRedirectMixin,
     list_per_page = 10
     date_hierarchy = 'modified'
     empty_value_display = '-'
-    next_form_getter_cls = NextFormGetter
 
-
-class CareerDevelopmentAdmin(TabularInlineMixin, admin.TabularInline):
-
-    model = CareerDevelopment
-    form = CareerDevelopmentForm
-    extra = 1
-
-    fieldsets = (
-        (None, {
-            'fields': [
-                'training_needs',
-                'agreed_action',
-                'review_date',
-                'completion_date',
-                'development_comments']}
-         ),)
-
-
-class ImprovementPlanAdmin(TabularInlineMixin, admin.TabularInline):
-
-    model = ImprovementPlan
-    form = ImprovementPlanForm
-    extra = 1
-
-    fieldsets = (
-        (None, {
-            'fields': [
-                'weakness',
-                'action_to_take',
-                'action_by',
-                'review_date',
-                'completed_date']}
-         ),)
-
-
-class PerformanceAssessmentItemAdmin(TabularInlineMixin, admin.TabularInline):
-
-    model = PerformanceAssessmentItem
-    form = PerformanceAssessmentItemForm
-    extra = 1
-
-    fieldsets = (
-        (None, {
-            'fields': [
-                'kpa_nd_objective',
-                'performance_indicators',
-                'weighting',
-                'mid_year_performance',
-                'kpa_rating',
-                'kpa_score',
-                'year_end_assessment']}
-         ),)
+    def redirect_url(self, request, obj, post_url_continue=None):
+        redirect_url = super().redirect_url(
+            request, obj, post_url_continue=post_url_continue)
+        if request.GET.dict().get('next'):
+            url_name = request.GET.dict().get('next').split(',')[0]
+            attrs = request.GET.dict().get('next').split(',')[1:]
+            options = {k: request.GET.dict().get(k)
+                       for k in attrs if request.GET.dict().get(k)}
+            try:
+                redirect_url = reverse(url_name, kwargs=options)
+            except NoReverseMatch as e:
+                raise ModelAdminNextUrlRedirectError(
+                    f'{e}. Got url_name={url_name}, kwargs={options}.')
+        return redirect_url
 
 
 @admin.register(PerformanceAssessment, site=contract_admin)
@@ -96,20 +54,18 @@ class PerformanceAssessmentAdmin(
 
     form = PerformanceAssessmentForm
 
-    inlines = [PerformanceAssessmentItemAdmin,
-               CareerDevelopmentAdmin, ImprovementPlanAdmin,]
-
     formfield_overrides = {
         models.TextField: {'widget': Textarea(
-            attrs={'rows': 1,
-                   'cols': 40,
-                   'style': 'height: 1em;'})},
+            attrs={'rows': 5,
+                   'cols': 50,
+                   'style': 'height: 3em;'})},
     }
 
     fieldsets = (
         (None, {
             'fields': (
-                'identifier',
+                'emp_identifier',
+                'contract',
                 'overall_perf_score',
                 'comp_nd_pro_score',
                 'final_assess_score',
@@ -119,28 +75,4 @@ class PerformanceAssessmentAdmin(
                 'manager_signature',
                 'date_manager_signed',
             )}),
-        ('COMPETENCIES AND PROFESSIONAL SKILLS ASSESSMENT', {
-            'fields': (
-                'strategic_orientation',
-                'strategic_orientation_comm',
-                'results_focus',
-                'results_focus_comm',
-                'leadership_motivation',
-                'leadership_motivation_comm',
-                'innovation_creativity',
-                'innovation_creativity_comm',
-                'planning_skills',
-                'planning_skills_comm',
-                'interpersonal_skills',
-                'interpersonal_skills_comm',
-                'communication_skills',
-                'communication_skills_comm',
-                'productivity',
-                'productivity_comm',
-                'quality_of_work',
-                'quality_of_work_comm',
-            )}),
         audit_fieldset_tuple)
-
-    search_fields = ['identifier', 'employee_code', 'agreed_by',
-                     'approved_by']
--- contract/forms/performance_assessment_form.py
+++ contract/forms/performance_assessment_form.py
@@ -7,6 +7,20 @@ from ..models import PerformanceAssessment
 
 class PerformanceAssessmentForm(SiteModelFormMixin, forms.ModelForm):
 
+    def clean(self):
+        super().clean()
+
+        performance_assessment_item = self.data.get(
+            'performanceassessmentitem_set-TOTAL_FORMS')
+
+        overall_perf_score = 0
+        for i in range(int(performance_assessment_item)):
+            kpa_rating = self.data.get(
+                'performanceassessmentitem_set-' + str(i) +'-kpa_rating'
+            )
+            import pdb; pdb.set_trace()
+            overall_perf_score = overall_perf_score + int(kpa_rating)
+
     class Meta:
         model = PerformanceAssessment
         fields = '__all__'
