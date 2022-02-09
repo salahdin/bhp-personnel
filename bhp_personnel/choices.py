f01a9f5a88c9e31599924ffcfdc31e9678113cad
samKenpachi011 <samuelkabelo1@gmail.com>
2022-02-09 08:24:56 +0200

    updated employee model and added a function to send the supervisor a notification

--- bhp_personnel/choices.py
+++ bhp_personnel/choices.py
@@ -1,3 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+from edc_constants.constants import OTHER
+
 CONTRACT_LENGTH = (
     ('2 Years', '2 Years'),
     ('1 Year', '1 Year'),
@@ -22,3 +25,32 @@ ASSESSMENT_TYPE = (
     ('mid_year', 'Mid Year Assessment'),
     ('year_end', 'Year End Assessment'),
     ('contract_end', 'End of Contract Assessment'))
+
+SALUTATIONS = (
+    ('mr','Mr'),
+    ('ms','Ms'),
+    ('mrs','Mrs'),
+)
+
+IDENTITY_TYPE = (
+    ('country_id', 'Country ID number'),
+    ('passport', 'Passport'),
+    ('birth_certificate', 'Birth Certificate'),
+    (OTHER, _('Other')),
+)
+
+HIGHEST_QUALIFICATION = (
+    ('None', 'None'),
+    ('primary', 'Primary'),
+    ('junior_secondary', 'Junior Secondary'),
+    ('senior_Secondary', 'Senior Secondary'),
+    ('diploma', 'Diploma'),
+    ('graduate_certificate', 'Graduate certificate'),
+    ('associates_degree', 'Associates degree'),
+    ('graduate_diploma', 'Graduate diploma'),
+    ('tertiary', 'Tertiary'),
+    ('bachelor_degree', 'Bachelor degree'),
+    ('masters_degree', 'Masters degree'),
+    ('doctoral_degree', 'Doctoral degree'),
+    ('phd', 'PhD'),)
+
--- bhp_personnel/models/employee.py
+++ bhp_personnel/models/employee.py
@@ -5,6 +5,16 @@ from edc_base.model_mixins import BaseUuidModel
 from edc_base.model_validators import CellNumber
 from edc_base.sites.site_model_mixin import SiteModelMixin
 from edc_search.model_mixins import SearchSlugModelMixin as Base
+from edc_base.model_validators.dob import dob_not_future,dob_not_today
+from edc_base.model_validators import datetime_not_future, date_is_future
+from django_crypto_fields.fields import IdentityField
+from edc_constants.choices import YES_NO_NA
+from edc_constants.constants import NOT_APPLICABLE
+from edc_base.model_validators import CellNumber
+from django_countries.fields import CountryField
+from django_crypto_fields.fields import EncryptedCharField
+
+from ..choices import SALUTATIONS, HIGHEST_QUALIFICATION, IDENTITY_TYPE
 
 from .model_mixins import CommonDetailsMixin
 from .department import Department
@@ -57,6 +67,63 @@ class Employee(CommonDetailsMixin, SiteModelMixin, SearchSlugModelMixin,
                BaseUuidModel):
 
     identifier_cls = EmployeeIdentifier
+
+    date_of_birth = models.DateField(
+        verbose_name="Date of birth",
+        validators=[dob_not_today],)
+
+    identity = IdentityField(
+        verbose_name='Identity number')
+
+    identity_type = models.CharField(
+        max_length=20,
+        verbose_name='What type of identity number is this?',
+        choices=IDENTITY_TYPE,)    
+
+    hired_date = models.DateField(
+        verbose_name='Hired Date')
+
+    contact = EncryptedCharField(
+        verbose_name='Contact Number',
+        validators=[CellNumber, ],
+        blank=False,
+        unique=True)
+
+    next_of_kin_contact = EncryptedCharField(
+        verbose_name='Next of Kin Contacts',
+        validators=[CellNumber, ],
+        blank=True,
+        null=True,)  
+
+    title_salutation = models.CharField(
+        verbose_name='Title',
+        choices=SALUTATIONS,      
+        max_length=4,)      
+
+    highest_qualification = models.CharField(
+        verbose_name='Highest qualification level',
+        choices= HIGHEST_QUALIFICATION,
+        max_length=30,)
+
+    nationality = models.CharField(
+        verbose_name='Is the employee a citizen of Botswana',
+        default=NOT_APPLICABLE,
+        choices=YES_NO_NA,
+        max_length=3,)
+
+    country = CountryField()    
+
+    postal_address = models.CharField(
+        verbose_name='Postal Address',
+        blank=True,
+        null=True,
+        max_length=120,)
+
+    physical_address = models.CharField(
+        verbose_name='Physical Address',
+        blank=True,
+        null=True,
+        max_length=120,)
 
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
