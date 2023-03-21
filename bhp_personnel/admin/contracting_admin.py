f02b1128054731cddac1b55f70b03738af9ba512
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2023-03-21 15:04:40 +0200

    fix(Signals): fix sending email when contract period is up

--- bhp_personnel/admin/contracting_admin.py
+++ bhp_personnel/admin/contracting_admin.py
@@ -43,7 +43,6 @@ class ContractingAdmin(ModelAdminMixin, admin.ModelAdmin):
         (None, {
             'fields': (
                 'identifier',
-                'contract',
                 'job_description',
                 'skills',
             )}),
