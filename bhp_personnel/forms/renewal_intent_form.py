04f885e96ea0d0743a59d1d692ef9807460761b4
salahdin <salahdinjemal@gmail.com>
2023-05-19 11:47:06 +0200

    refactor

--- bhp_personnel/forms/renewal_intent_form.py
+++ bhp_personnel/forms/renewal_intent_form.py
@@ -2,7 +2,7 @@ from django import forms
 
 from edc_base.sites import SiteModelFormMixin
 
-from ..models import RenewalIntent, Contract
+from ..models import RenewalIntent, Contract, Employee
 
 
 class RenewalIntentForm(SiteModelFormMixin, forms.ModelForm):
@@ -12,10 +12,22 @@ class RenewalIntentForm(SiteModelFormMixin, forms.ModelForm):
     )
 
     def __init__(self, *args, **kwargs):
-        contract = self.request.GET.get('contract')
         super().__init__(*args, **kwargs)
-        if contract:
-            self.fields['contract'].queryset = Contract.objects.filter(id=contract)
+
+        contract_id = self.request.GET.get('contract')
+        if contract_id:
+            contract = Contract.objects.filter(id=contract_id).first()
+            if contract:
+                self.fields['contract'].queryset = Contract.objects.filter(id=contract.id)
+                employee = Employee.objects.filter(identifier=contract.identifier).first()
+                if employee:
+                    is_employee = self.request.user.email == employee.email
+                    is_supervisor = self.request.user.email == employee.supervisor.email
+                    if not is_employee:
+                        self.fields['intent'].disabled = True
+                        self.fields['letter_upload'].disabled = True
+                    elif not is_supervisor:
+                        self.fields['comment'].disabled = True
 
     class Meta:
         model = RenewalIntent
